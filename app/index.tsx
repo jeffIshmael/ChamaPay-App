9ff1bfb4f6b903a9f3ded6c6f4851384fe1cc367
John Njuki <johnnjuki.dev@gmail.com>
2025-07-28 07:41:17 +0300

    redirect to the onboarding by default

--- app/index.tsx
+++ app/index.tsx
@@ -2,5 +2,5 @@ import { Redirect } from "expo-router";
 
 export default function Index() {
   // Redirect to onboarding by default
-  return <Redirect href="/(tabs)" />;
+  return <Redirect href="/onboarding" />;
 }
