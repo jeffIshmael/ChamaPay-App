d2d1621833dce20aba4fd0ebcbc070c68d48dce4
jeffIshmael <jeffishmael141@gmail.com>
2025-07-30 14:17:43 +0300

    update: adding branch

--- Server/Middlewares/authMiddleware.js
+++ Server/Middlewares/authMiddleware.js
@@ -4,7 +4,7 @@ const authenticate = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader)
     return res.status(401).json({ message: "Authorization required" });
-  const token = authHeader.split(" ")[1];
+    const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // Attach user ID from the token
