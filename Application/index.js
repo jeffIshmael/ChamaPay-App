8c891df15dd9c961b656f2f25d49ec0fda8215ac
jeffIshmael <jeffishmael141@gmail.com>
2026-01-08 22:09:20 +0300

    update : trying to build

--- Application/index.js
+++ Application/index.js
@@ -1,6 +1,10 @@
 import { Platform } from "react-native";
 import "react-native-get-random-values";
 
+// Install crypto polyfill FIRST
+import { install } from 'react-native-quick-crypto';
+install();
+
 // Add polyfills for Node.js modules
 if (typeof global.Buffer === 'undefined') {
   global.Buffer = require('buffer').Buffer;
@@ -8,6 +12,12 @@ if (typeof global.Buffer === 'undefined') {
 
 if (typeof global.process === 'undefined') {
   global.process = require('process');
+}
+
+// Add crypto to global if not present
+if (typeof global.crypto === 'undefined') {
+  const { crypto } = require('react-native-quick-crypto');
+  global.crypto = crypto;
 }
 
 if (Platform.OS !== "web") {
